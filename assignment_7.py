2c65bee16b00965297c930db70a83da35ffa12f1
afro-creator745 <hacole@aggies.ncat.edu>
2026-02-27 22:08:23 -0500

    Update assignment_7.py
    Completed step 1 of the course organizer

--- assignment_7.py
+++ assignment_7.py
@@ -11,6 +11,49 @@ AI Usage: [Describe any AI assistance OR write "None"]
 # ============================================================
 # Step 1: Input Parsing & Course Code Formatting
 # ============================================================
+day_full = {"M": "Monday", "T": "Tuesday", "W": "Wednesday", "R": "Thursday", "F": "Friday"}
+day_abbr = {"M": "Mon", "T": "Tue", "W": "Wed", "R": "Thu", "F": "Fri"}
+day_order = ["M", "T", "W", "R", "F"]
+
+course_info = input("Enter course data: ").strip()
+index = 1
+course_dictionary = {}
+
+while course_info.upper() != "DONE":
+    course_list = course_info.split("|")
+
+    # Ensure 5 fields
+    while len(course_list) < 5:
+        course_list.append("")
+
+    # Strip each field
+    for i in range(5):
+        course_list[i] = course_list[i].strip()
+
+    course_dictionary[index] = course_list
+    index += 1
+    course_info = input("Enter course data: ").strip()
+
+print()
+print("=== AGGIE COURSE SCHEDULE ===")
+print()
+
+# We'll store cleaned courses here for conflicts + formatted output
+clean_courses = []
+
+for idx in course_dictionary:
+    current_course = course_dictionary[idx]
+
+    print("COURSE", f"{idx}:")
+    print()
+
+    # ---------------------------
+    # Code formatting
+    # ---------------------------
+    class_code = current_course[0]
+    class_code_clean = " ".join(class_code.replace("-", " ").upper().split())
+    print("  Code:", class_code_clean)
+    print()
 
 
 # ============================================================
